59c2111b9c577dbd412880a25bbe8c02e2a21f48
Toqa <90261037+Toqaalsourouji@users.noreply.github.com>
2024-12-25 02:59:38 +0200

    Create RISC-V-Pipeline_tb.v

---
+++ RISC-V-Pipeline_tb.v
@@ -0,0 +1,22 @@
+module RISC_V_TB();
+reg clk, ssdclk, reset, load, ledSel, ssdSel;
+wire leds, ssd;
+
+RISCV_Pipeline DUT ( clk, ssdclk, reset, load, ledSel,  ssdSel, leds,  ssd);
+
+initial begin 
+clk = 0 ;
+forever #5 clk = ~ clk;
+end 
+
+initial begin 
+reset = 1 ; 
+load = 0 ;
+#10 
+reset = 0 ;
+load = 1 ;
+#500
+$finish;
+end
+
+endmodule
